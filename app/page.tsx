81ba6174d69bed519e28bdebf8fd18d9c507dda3
faraz9012 <54522532+faraz9012@users.noreply.github.com>
2023-10-25 19:35:42 +0500

    [feat]: hero section created [style]: assets imported

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from '@/components/Hero'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <h1 className='text-center'>Hello World</h1>
+      <Hero />
     </main>
   )
 }
