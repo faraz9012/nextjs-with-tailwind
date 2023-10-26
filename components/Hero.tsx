26e7957d3606b872ce61e3502f8a756828df2403
faraz9012 <54522532+faraz9012@users.noreply.github.com>
2023-10-26 12:24:47 +0500

    header, footer, hero section completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,11 +30,9 @@ const Hero = () => {
             <div className='hero__image'>
                 <Image src='/hero.png' alt='hero'
                 fill className='object-contain'/>
-
-                <div className='hero__image-overlay'>
-
-                </div>
             </div>
+
+            <div className='hero__image-overlay' />
         </div>
     </div>
   )
