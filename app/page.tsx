ccfa3c08cf34acdba940722034c2a546ad10363e
faraz9012 <54522532+faraz9012@users.noreply.github.com>
2023-10-27 18:00:01 +0500

    [feat]: custom filters added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 
 import { CustomFilter, SearchBar,Hero, CarCard } from '@/components'
 import { fetchCars } from '@/utils'
+import { fuels, yearsOfProduction } from '@/constants';
 
 export default async function Home({searchParams}) {
   const allCars = await fetchCars({
@@ -29,8 +30,8 @@ export default async function Home({searchParams}) {
           
           <div
             className='home__filter-container'>
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
