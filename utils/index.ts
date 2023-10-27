1d4b1ae53b0970fa0d2d237809188074edd53e12
faraz9012 <54522532+faraz9012@users.noreply.github.com>
2023-10-27 16:38:17 +0500

    search filter completed

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,15 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars(){
+export async function fetchCars(filters: FilterProps){
+
+  const {manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     'X-RapidAPI-Key': '46d4e2ab5bmsh352c0ae53cd6f7bp138707jsn54b2d15bb717',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
 
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
+  const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
     headers:headers,
   });
 
